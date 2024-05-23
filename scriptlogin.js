f96d7ae50575873bba647e91522e9a7a8f9fdc25
Humberto <jesushumbertoricomejia1@gmail.com>
2024-05-22 22:53:47 -0600

    update del login

--- scriptlogin.js
+++ scriptlogin.js
@@ -1,32 +1,35 @@
 /*esta es la parte del login */
 const url = "http://localhost:3000/usuarios";
-const emailvalor = document.getElementById('email');
-const passworvalor = document.getElementById('contrasena');
+const emailvalor = document.getElementById("email");
+const passworvalor = document.getElementById("contrasena");
 
-const btn_login = document.getElementById('submit');
+const btn_login = document.getElementById("submit");
 
-btn_login.addEventListener('click', function (event) {
-    event.preventDefault();
-   
-    
-    fetch('http://localhost:3000/usuarios')
-    .then((res)=>res.json())
+btn_login.addEventListener("click", function (event) {
+  event.preventDefault();
+
+  fetch("http://localhost:3000/usuarios")
+    .then((response) => response.json())
     .then((data) => {
-     let r=data.find((item)=>{
-      if(item.email == emailvalor.value){
-        if(item.contrasena == passworvalor.value){
-          location.href='index.html';
-        }
-        
-        
-  
-      }
-      
-     });
-     
-    
-      
-    } );
-});
 
+      console.log(data);
+      let user = data.find((item) => item.email === email.value);
+      let pasword = data.find(
+        (item) => item.password === passworvalor.value
+      );
+
+      if (user.email == email.value) {
+        console.log("entro en email");
+        if (pasword.password == passworvalor.value) {
+          console.log("entro en pasword");
+          location.href = "index.html";
+        }
+      } else {
+      }
+    })
+    .catch((error) => {
+      console.log(error);
+      // Manejar el caso en que hay un error al fetch o parsear el JSON
+    });
+});
 
